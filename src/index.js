18bad99bd0d8d3985692147bd532fa7073287759
Jason Loomis <jason@jrloomis.com>
2019-12-17 18:07:59 -0500

    import, setup router and apollo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  //uri:
+  //todo insert url for backend
+});
+
+ReactDOM.render(
+  <Router>
+    <ApolloClient client={client}>
+      <App />
+    </ApolloClient>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

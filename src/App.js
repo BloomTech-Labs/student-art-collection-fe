d7479e14327b9a707ab75a6787fae21b50f95cf6
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-20 14:54:30 -0800

    Now rendering data from backend, pushing to staging to ensure that everything is working before a merge to production
    Co-authored-by: Dave Inden <davidinden@gmail.com>
    Co-authored-by: Jason Loomis <jason@jrloomis.com>
    Co-authored-by: Grissobel Payonk <grissobel.castro@gmail.com>
    Co-authored-by: Ian Schwartz <ian.schwartz23@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { AuthProvider } from './components/auth/Auth';
 import { Register } from './components'
 import { Login } from './components'
+import { Images } from './components/images/images'
 
 
 function App() {
   return (
     <div>
+      <p>hi</p>
       <AuthProvider>
         <Register />
         <Login />
       </AuthProvider>
+      <Images />
     </div>
   );
 }
---
+++ src/components/images/images.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {gql} from 'apollo-boost';
+import {useQuery} from 'react-apollo';
+
+const GET_IMAGES = gql`
+query {
+    allImages {
+      id
+      image_url
+    }
+  }
+`
+
+export const Images = () => {
+    const{loading, error, data} = useQuery(GET_IMAGES)
+   
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    } 
+    if (error) {
+        return (
+            <div>Error...</div>
+        )
+    } 
+    if (data) {
+        // using this instead of useState as the latter causes an infinite loop
+        const images = data.allImages
+        
+        return (
+        <>
+        {images.map((item) => (
+            <img key={item.id} src={item.image_url} alt="gallery working" />
+        )
+        )}
+        </>
+        )
+    }
+}
+

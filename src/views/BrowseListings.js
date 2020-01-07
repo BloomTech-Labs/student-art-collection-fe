324bc76fa5d5fc076633e97dfba5d6368bb064e4
Jason Loomis <jason@jrloomis.com>
2020-01-07 16:48:16 -0500

    header layout - needs some tweaking for styling, and the navbar

--- src/views/BrowseListings.js
+++ src/views/BrowseListings.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
 
 // import { Navigation } from '../components'
 
+//todo uncomment { Navigation } import and <Navigation /> when ready
+//todo heading should be all caps i.e. text-transform: uppercase
+
 const BrowseListings = () => {
   return (
-    <div>
-      {/* <Navigation /> */}
-      <div>DIV!</div>
-    </div>
+    <Grid container direction='column'>
+      <Grid item component='header'>
+        <Grid container direction='column' spacing={5}>
+          <Grid item>
+            {/* <Navigation /> */}
+            <div>Placeholder for navbar</div>
+          </Grid>
+          <Grid item>
+            <Typography variant='h1' component='h1' gutterBottom={true}>
+              Welcome
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>Images go here</Grid>
+    </Grid>
   )
 }
 

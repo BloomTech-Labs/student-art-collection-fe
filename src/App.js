baf0c0db04219a9617b7240eb032f217a518374f
dAVE Inden <davidinden@gmail.com>
2020-01-08 15:43:32 -0800

    import PrivateRoute to App.js and set example usage in comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 import { AuthProvider } from './components/auth/Auth'
+import PrivateRoute from './components/auth/PrivateRoute'
 import { Register } from './components'
 import { Login } from './components'
 // import { Images } from './components/images/images'
@@ -18,6 +19,8 @@ function App() {
             <Route path='/login' component={Login} />
             <Route path='/register' component={Register} />
             <Route path='/browse' component={BrowseListings} />
+            {/* Example PrivateRoute usage */}
+            {/* <PrivateRoute path='<PATH_FOR_ROUTE>' component={'<COMPONENT_FOR_ROUTE>'} /> */}
           </Switch>
         </Router>
       </AuthProvider>

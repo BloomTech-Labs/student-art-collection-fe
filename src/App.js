fc4c586a80c00c9b20c676e14e0320137e04ed85
dAVE Inden <davidinden@gmail.com>
2020-01-08 15:12:16 -0800

    setup router and initial routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 import { AuthProvider } from './components/auth/Auth'
-// import { Register } from './components'
-// import { Login } from './components'
+import { Register } from './components'
+import { Login } from './components'
 // import { Images } from './components/images/images'
 import { BrowseListings } from './views'
 import Contact from './components/forms/Contact.js';
@@ -10,8 +11,15 @@ import Contact from './components/forms/Contact.js';
 function App() {
   return (
     <Container>
-      <AuthProvider></AuthProvider>
-      <BrowseListings />
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route path='/browse' component={BrowseListings} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </Container>
   )
 }

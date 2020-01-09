12f9e9c9bdc3fdbe56f91b0097243d902d12b341
Jason Loomis <jason@jrloomis.com>
2020-01-08 19:59:45 -0500

    import/export contact form

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Register from './forms/Register'
 import Login from './forms/Login'
 import Navigation from './Navigation'
 import ImageMasonry from './ImageMasonry'
+// import ImageCard from './ImageCard'
+import Contact from './forms/Contact'
 
-export { Register, Login, Navigation, ImageMasonry }
+export { Register, Login, Navigation, ImageMasonry, Contact }

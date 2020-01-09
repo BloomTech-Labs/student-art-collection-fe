24af00ac37a08dcfc99d6193a4be561139d1588c
Jason Loomis <jason@jrloomis.com>
2020-01-09 11:54:59 -0500

    updated with ArtInfo and ImageCarousel

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
 import Register from './forms/Register'
 import Login from './forms/Login'
+import Contact from './forms/Contact'
 import Navigation from './Navigation'
 import ImageMasonry from './ImageMasonry'
-// import ImageCard from './ImageCard'
-import Contact from './forms/Contact'
+import ImageCarousel from './ImageCarousel'
+import ArtInfo from './ArtInfo'
 
-export { Register, Login, Navigation, ImageMasonry, Contact }
+export {
+  Register,
+  Login,
+  Contact,
+  Navigation,
+  ImageMasonry,
+  ImageCarousel,
+  ArtInfo,
+}

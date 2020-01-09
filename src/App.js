471f5814a3763785e499df9e4fde93b6fbe67ef3
Jason Loomis <jason@jrloomis.com>
2020-01-08 19:58:53 -0500

    changes made to be able to see current work - will be changed back to original when done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { AuthProvider } from './components/auth/Auth'
 // import { Register } from './components'
 // import { Login } from './components'
 // import { Images } from './components/images/images'
-import { BrowseListings } from './views'
-import Contact from './components/forms/Contact.js';
+// import { BrowseListings } from './views'
+import { SinglePage } from './views'
 
 function App() {
   return (
     <Container>
       <AuthProvider></AuthProvider>
-      <BrowseListings />
+      {/* <BrowseListings /> */}
+      <SinglePage />
     </Container>
   )
 }

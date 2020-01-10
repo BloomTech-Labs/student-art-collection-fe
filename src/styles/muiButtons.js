35778db708dc7c0edca31142c90438f5efc67d26
Jason Loomis <jason@jrloomis.com>
2020-01-09 21:09:35 -0500

    removed hover effect - using mui IconButton instead

--- src/styles/muiButtons.js
+++ src/styles/muiButtons.js
@@ -7,11 +7,5 @@ export const BackButton = withStyles({
     padding: theme.spacing(1.5),
     backgroundColor: 'rgba(0,0,0,.1)',
     borderRadius: '50%',
-    '&:hover': {
-      backgroundColor: 'rgba(0,0,0,.2)',
-      transition: theme.transitions.create('background-color', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
   },
 })(ArrowBackIcon)

c6315dfdc2b563c52aaa11eee7ff43fb3782c6a2
Jason Loomis <jason@jrloomis.com>
2020-01-09 20:18:13 -0500

    added global style for back button

--- src/styles/muiButtons.js
+++ src/styles/muiButtons.js
@@ -1,8 +1,17 @@
 import { withStyles } from '@material-ui/core'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { theme } from './theme'
 
 export const BackButton = withStyles({
   root: {
-    padding: '20px',
+    padding: theme.spacing(1.5),
+    backgroundColor: 'rgba(0,0,0,.1)',
+    borderRadius: '50%',
+    '&:hover': {
+      backgroundColor: 'rgba(0,0,0,.2)',
+      transition: theme.transitions.create('background-color', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
   },
 })(ArrowBackIcon)

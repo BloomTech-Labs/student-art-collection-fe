d9364a823c9eae5ba7da5a2483d3b647c46898e7
Jason Loomis <jason@jrloomis.com>
2020-01-13 14:02:13 -0500

    removed unused imports, added admin artwork to private, grouped private groups

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 import { AuthProvider } from './components/auth/Auth'
 import PrivateRoute from './components/auth/PrivateRoute'
-// import { Images } from './components/images/images'
-import Contact from './components/forms/Contact.js'
 import Dashboard from './components/Dashboard'
 import { Register, Login, Navigation } from './components'
 import { BrowseListings, MainPage, SinglePage, AdminSingleView } from './views'
@@ -20,16 +18,14 @@ function App() {
             <Route path='/login' component={Login} />
             <Route path='/register' component={Register} />
             <Route path='/browse' component={BrowseListings} />
-            {/* <Route path='/dashboard' component={Dashboard} /> */}
-            <PrivateRoute path='/dashboard' component={Dashboard} />
             <Route path='/artwork/:id' component={SinglePage} />
-            <Route path='/admin/artwork/:id' component={AdminSingleView} />
-            {/* <Route
-              path='/artwork'
-              render={props => <SinglePage {...props} id={1} />}
-            /> */}
             {/* Example PrivateRoute usage */}
             {/* <PrivateRoute path='<PATH_FOR_ROUTE>' component={'<COMPONENT_FOR_ROUTE>'} /> */}
+            <PrivateRoute path='/dashboard' component={Dashboard} />
+            <PrivateRoute
+              path='/admin/artwork/:id'
+              component={AdminSingleView}
+            />
           </Switch>
         </Router>
       </AuthProvider>

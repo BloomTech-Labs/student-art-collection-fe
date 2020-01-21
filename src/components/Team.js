329d0df1875438551d12c6eb61cbc96dac5ba02f
gspayonk <grissobel.castro@gmail.com>
2020-01-21 16:30:17 -0500

    Adding sections

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { CardMedia } from '@material-ui/core'
+
 
 const Team = () => {
     return (
-        <section>
+        <section className='top-section'>
+            <image src={'../images/unsplash-art-1.jpg'}/>
+            <h2>Meet Our Team</h2>
         
         </section>
     )

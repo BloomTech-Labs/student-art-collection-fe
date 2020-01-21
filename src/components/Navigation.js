05ee915dc0c27edd4b11d4a65a93aa034b6a3a9f
Ian Schwartz <ian.schwartz23@gmail.com>
2020-01-21 09:29:23 -0800

    hide on scroll and scroll to top button (not yet functional) added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,10 @@ import { Grid, Button, AppBar, makeStyles } from '@material-ui/core'
 import firebaseApp from './auth/firebaseApp'
 import { AuthContext } from './auth/Auth'
 import logo from '../images/logo1.png'
+import HideOnScroll from './HideOnScroll'
+// import ScrollTop from './ScrollTop'
+// import Fab from '@material-ui/core/Fab';
+// import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 const useStyles = makeStyles(theme => ({
   // space: {
@@ -13,19 +17,19 @@ const useStyles = makeStyles(theme => ({
   logo: {
     textDecoration: 'none',
     // color: '#000',
-    padding: '10px'
+    padding: '10px',
   },
   button: {
     color: '#fff',
-    borderColor: '#fff'
+    borderColor: '#fff',
   },
   appbar: {
     background: '#000',
-    height: '8%', 
-  }
+    // height: '8%',
+  },
 }))
 
-const Navigation = () => {
+const Navigation = props => {
   const classes = useStyles()
   const history = useHistory()
   const { authenticated } = useContext(AuthContext)
@@ -41,61 +45,86 @@ const Navigation = () => {
   }
 
   return (
-   <AppBar className={classes.appbar}>
-    <Grid
-      container
-      alignItems='center'
-      justify='space-between'
-      className={classes.space}
-    >
-      <Grid item>
-        <Link to='/' className={classes.logo}>
-          <img src={logo} alt='Student Artco' />
-        </Link>
-      </Grid>
-      <Grid item>
-        <Grid container spacing={5}>
-          {authenticated === true ? (
-            <Grid item>
-              <Button component={Link} to='/admin/dashboard' className={classes.button}>
-                Dashboard
-              </Button>
+    <HideOnScroll {...props}>
+      <AppBar className={classes.appbar}>
+        <Grid
+          container
+          alignItems='center'
+          justify='space-between'
+          className={classes.space}
+        >
+          <Grid item>
+            <Link to='/' className={classes.logo}>
+              <img src={logo} alt='Student Artco' />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={5}>
+              <Grid item>
+                <Button
+                  component={Link}
+                  to='/browse'
+                  className={classes.button}
+                >
+                  Browse
+                </Button>
+              </Grid>
+              {authenticated === true ? (
+                <Grid item>
+                  <Button
+                    component={Link}
+                    to='/admin/dashboard'
+                    className={classes.button}
+                  >
+                    Dashboard
+                  </Button>
+                </Grid>
+              ) : null}
+              <Grid item>
+                {authenticated === true ? (
+                  <Button
+                    onClick={signOut}
+                    variant='outlined'
+                    className={classes.button}
+                  >
+                    Sign Out
+                  </Button>
+                ) : (
+                  <Button
+                    component={Link}
+                    to='/login'
+                    className={classes.button}
+                  >
+                    Sign In
+                  </Button>
+                )}
+              </Grid>
+              <Grid item>
+                {authenticated === true ? (
+                  <Button disabled />
+                ) : (
+                  <Button
+                    component={Link}
+                    to='/register'
+                    variant='outlined'
+                    disableElevation
+                    className={classes.button}
+                  >
+                    Join
+                  </Button>
+                )}
+                <Button disabled />
+              </Grid>
             </Grid>
-          ) : null}
-          <Grid item>
-            <Button component={Link} to='/browse' className={classes.button}>
-              Browse
-            </Button>
-          </Grid>
-          <Grid item>
-            {authenticated === true ? (
-              <Button onClick={signOut} variant='outlined' className={classes.button}>Sign Out</Button>
-            ) : (
-              <Button component={Link} to='/login' className={classes.button}>
-                Sign In
-              </Button>
-            )}
-          </Grid>
-          <Grid item>
-            {authenticated === true ? (
-              <Button disabled />
-            ) : (
-              <Button
-                component={Link}
-                to='/register'
-                variant='outlined'
-                disableElevation
-                className={classes.button}
-              >
-                Join
-              </Button>
-            )}
-            <Button disabled />
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-    </AppBar>
+      </AppBar>
+      {/* <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop> */}
+    </HideOnScroll>
   )
 }
 
---
+++ src/components/ScrollTop.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Zoom from '@material-ui/core/Zoom'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}))
+
+const ScrollTop = props => {
+  const { children, window } = props
+  const classes = useStyles()
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  })
+
+  const handleClick = e => {
+    const anchor = (e.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    )
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+  }
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role='presentation' className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  )
+}
+
+export default ScrollTop

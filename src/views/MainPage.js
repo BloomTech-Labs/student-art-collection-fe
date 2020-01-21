853e92235c19916415250acd50b50e73a69b8ab7
dAVE Inden <davidinden@gmail.com>
2020-01-21 10:10:45 -0800

    initial imports and basic page skeleton

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { styles as makeStyles, Grid } from '@material-ui/core';
 
 const MainPage = () => {
     return (
-        <div>
-            <h1>Student Art Co</h1>
-            <p>Placeholder for marketing content.</p>
-        </div>
+        <>
+            <div>
+                <h1>Student Art Co</h1>
+                <p>Welcome message with links for call to action</p>
+            </div>
+            <div>
+                <p>Explanation of how the application works</p>
+            </div>
+            <div>
+                <p>Explanation of how one can sign up to use the application for their school</p>
+            </div>
+            <footer>
+                <p>Site Footer</p>
+            </footer>
+        </>
     )
 }
 

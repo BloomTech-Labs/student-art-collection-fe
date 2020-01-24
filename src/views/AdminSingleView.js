f8a007ffc6fc3338de752a93567b62c2aca4e928
Ian Schwartz <ian.schwartz23@gmail.com>
2020-01-24 11:08:17 -0800

    margin adjusted

--- src/views/AdminSingleView.js
+++ src/views/AdminSingleView.js
@@ -91,7 +91,7 @@ const AdminSingleView = props => {
   }
   if (data) {
     return (
-        <Container style={{ marginTop: '150px', marginBottom: '100px' }}>
+        <Container style={{ marginTop: '100px', marginBottom: '100px' }}>
           <Card>
             <CardActions>
               <IconButton

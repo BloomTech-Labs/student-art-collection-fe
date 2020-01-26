ff270a182e813271dac894384d70f0780e07e636
gspayonk <grissobel.castro@gmail.com>
2020-01-26 17:20:15 -0500

    Updates to styling of about page.

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,22 +1,162 @@
 import React from 'react'
-import { CardMedia } from '@material-ui/core'
-import FoxArt from '../images/unsplash-art-1.jpg'
-import styled from 'styled-components'
-
-const FoxImg = styled.image `
-    background-image:  src={FoxArt}
-`
-
+// import { CardMedia, Card, CardActionArea } from '@material-ui/core'
+// import FoxArt from '../images/unsplash-art-1.jpg'
+// import styled from 'styled-components'
+import './Team.css'
 
 const Team = () => {
     return (
-        <section className='top-section'>
-            <div>
-            <img src={FoxArt} alt='fox art img' width='100%' height='1%'/>
-            </div>
-            
-            <h2>Meet Our Team</h2>
-        </section>
+        <>
+<section>
+	<div className='container'>
+
+    <div className="pull-left col-md-4 sm-text-center">
+				<div className='team-overview'>
+					<h2>Who Are We?</h2>
+					<p>
+                        <div>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quae nesciunt officia! Autem, nam eum voluptatem, esse voluptatibus deleniti tempora laboriosam sequi pariatur, explicabo itaque. Fuga, deserunt? Repudiandae, neque qui.
+                        </div>
+                    </p>
+				</div>
+			</div>
+
+		<div className='row flex-center sm-no-flex'>
+
+			<div className='pull-right sm-no-float col-md-8'>
+				<ul className='team-members'>
+					<ul>
+						<div className='member-details'>
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-ULVE1AR4Y-32e4313617ec-512' alt='Web Developer' />
+								<div className="member-info">
+									<h3>dAVE Inden</h3>
+									<p>Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UFX4PAND7-053ac23153cf-512' alt='Web Developer' />
+								<div className="member-info">
+									<h3>Mackenzie Weber</h3>
+									<p>Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://avatars1.githubusercontent.com/u/40191456?s=400&v=4' alt='Web Developer'/>
+								<div className="member-info">
+									<h3>Jason Loomis</h3>
+									<p>Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+					</ul>
+
+					<ul>
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-ULV0CMZRS-3848f2627a12-512' alt='Web Developer'/>
+								<div className="member-info">
+									<h3>Grissobel Payonk</h3>
+									<p>Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-ULXK90YSJ-dceca3c6a285-512' alt='Web Developer'/>
+								<div className="member-info">
+									<h3>Ian Schwartz</h3>
+									<p>Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+
+						<div className="member-details">
+							<div>
+								<img src="https://ca.slack-edge.com/T4JUEB3ME-UDTA6SESD-f9033663ba71-512" alt='Web Developer'/>
+								<div className="member-info">
+									<h3>Ami Scott</h3>
+									<p>Team Lead-Full Stack</p>
+                                    <p>Web Developer</p>
+								</div>
+							</div>
+						</div>
+					</ul>
+
+		
+					<ul>
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UFK58KV38-e62a8bb2adc8-512' alt='iOS Developer'/>
+								<div className="member-info">
+									<h3>Mitchell Budge</h3>
+									<p>iOS Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UFAGBFNRK-f6066cbb23b3-512' alt='Mobile Developer'/>
+								<div className="member-info">
+									<h3>Vivek Vishwanath</h3>
+									<p>Software Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UE26GM3NC-35a35c655340-512' alt='Mobile Developer'/>
+								<div className="member-info">
+									<h3>Brandon Lively</h3>
+									<p>Mobile Developer</p>
+								</div>
+							</div>
+						</div>
+					</ul>
+
+                    <ul>
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UL6GJCZ6Y-b607c4f87820-512' alt='iOS Developer'/>
+								<div className="member-info">
+									<h3>Alexandra Rhodes</h3>
+                                    <p>iOS Developer</p>
+								</div>
+							</div>
+						</div>
+						
+						<div className="member-details">
+							<div>
+								<img src='https://ca.slack-edge.com/T4JUEB3ME-UEXB31TAP-2b07e714ffb3-512' alt='UI Designer'/>
+								<div className="member-info">
+									<h3>Imani Russ</h3>
+									<p>UI Designer</p>
+								</div>
+							</div>
+						</div>
+					</ul>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+        </>
     )
 }
 

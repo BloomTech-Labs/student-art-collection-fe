038da0529f0fab12b5ed96549f81126383b86773
Jason Loomis <jason@jrloomis.com>
2020-01-28 12:51:29 -0500

    slight changes

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLazyQuery } from 'react-apollo'
+import { useQuery } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
 //todo remove data checks
@@ -8,8 +8,8 @@ import { DataCheck } from './DataCheck'
 //todo including 'category' is causing this to fail
 //todo an issue with the resolver...???
 const SEARCH_ART = gql`
-  query searchArt($zipcode: String) {
-    filter(filter: { zipcode: { eq: $zipcode } }) {
+  query searchArt($zipcode: String, $category: String) {
+    filter(filter: { zipcode: { eq: $zipcode }, category: { eq: $category } }) {
       id
       title
       school {
@@ -28,45 +28,30 @@ const SEARCH_ART = gql`
     }
   }
 `
+
 const SearchResults = ({ location }) => {
   const { cat, zip } = location.state.newSearch
-  const [searchArt, { called, loading, data }] = useLazyQuery(SEARCH_ART, {
-    variables: { zipcode: zip },
+  const { error, loading, data } = useQuery(SEARCH_ART, {
+    variables: { zipcode: zip, category: cat },
   })
-
-  if (called && loading) {
+  if (error) {
     return (
       <>
-        <h1>Called and Loading!</h1>
-        <DataCheck data={{ category: cat }} />
-        <DataCheck data={{ zipcode: zip }} />
+        <DataCheck data={error} />
+        <h1>Error</h1>
       </>
     )
   }
-  if (!called) {
+  if (loading) {
+    return <h1>Loading</h1>
+  }
+  if (data) {
     return (
       <>
-        <h1>Not called!</h1>
-        {/* 
-        //todo automate this - user shouldn't need to click the button to see results
-        */}
-        <button onClick={() => searchArt()}>Load results</button>
-        <DataCheck data={{ category: cat }} />
-        <DataCheck data={{ zipcode: zip }} />
+        <DataCheck data={data} />
       </>
     )
   }
-  return (
-    <>
-      {/*
-      //todo format results page similar to other display pages (eg ImageMasonry)
-      //? Even better: A component like ImageMasonry should only be formatting image layout, not everything else it's doing...
-      */}
-      <DataCheck data={data} />
-      <DataCheck data={{ category: cat }} />
-      <DataCheck data={{ zipcode: zip }} />
-    </>
-  )
 }
 
 export default SearchResults

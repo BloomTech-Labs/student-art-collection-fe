54949ab460cb272f828976480acc73f27166f10c
Ian Schwartz <ian.schwartz23@gmail.com>
2020-01-30 12:30:14 -0800

    initial form fix for mobile responsiveness

--- src/components/forms/Submission.js
+++ src/components/forms/Submission.js
@@ -100,7 +100,7 @@ const Submission = props => {
   return (
     <Container style={{ marginTop: '50px', marginBottom: '50px' }}>
       <Paper elevation={0} className={classes.root}>
-        <Paper elevation={3} className={classes.paper}>
+        <Paper elevation={3} className={classes.paper} style={{ width: '450px' }}>
           <Grid container direction='column' alignItems='center' spacing={4}>
             <Grid item>
               <Typography variant='h2' component='h2'>

5d7728d8566b5b52fc373c984fe80275183f25b6
Jason Loomis <jason@jrloomis.com>
2020-01-30 15:02:32 -0500

    update components mostly functional, and styled

---
+++ src/components/forms/EditProfile.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useMutation } from 'react-apollo'
+import { Grid } from '@material-ui/core'
+import { formStyles, InputField } from '../../styles/muiForms'
+import { SubmitButton } from '../../styles/muiButtons'
+
+const UPDATE_SCHOOL = gql`
+  mutation updateSchool(
+    $school_name: String!
+    $email: String!
+    $address: String!
+    $city: String!
+    $zipcode: String!
+  ) {
+    updateSchool(
+      school_name: $school_name
+      email: $email
+      address: $address
+      city: $city
+      zipcode: $zipcode
+    ) {
+      school_name
+      email
+      address
+      city
+      zipcode
+    }
+  }
+`
+
+const EditProfile = ({ school }) => {
+  const [updateSchool] = useMutation(UPDATE_SCHOOL)
+  const classes = formStyles()
+  const [update, setUpdate] = useState({
+    school_name: school.school_name,
+    email: school.email,
+    address: school.address,
+    city: school.city,
+    zipcode: school.zipcode,
+  })
+
+  const handleChange = e =>
+    setUpdate({ ...update, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    const variables = { ...update }
+    console.log(`variables >>>`, variables)
+    updateSchool({
+      variables,
+    })
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        spacing={4}
+        className={classes.root}
+      >
+        <Grid item>
+          <InputField
+            name='school_name'
+            label='School Name'
+            variant='outlined'
+            size='small'
+            type='text'
+            value={update.school_name}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <InputField
+            name='email'
+            label='Email'
+            variant='outlined'
+            size='small'
+            type='text'
+            value={update.email}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <InputField
+            name='address'
+            label='Address'
+            variant='outlined'
+            size='small'
+            type='text'
+            value={update.address}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <InputField
+            name='city'
+            label='City'
+            variant='outlined'
+            size='small'
+            type='text'
+            value={update.city}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <InputField
+            name='zipcode'
+            label='Zip Code'
+            variant='outlined'
+            size='small'
+            type='text'
+            value={update.zipcode}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <SubmitButton variant='contained' size='medium' type='submit'>
+            Submit
+          </SubmitButton>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
+
+export default EditProfile
--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,18 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from 'react-apollo'
+import { makeStyles, Card, Grid, Typography } from '@material-ui/core'
+import { EditProfile, ErrorMessage, Spinner } from '../components'
+
+const GET_SCHOOL_INFO = gql`
+  query {
+    school(id: 1) {
+      school_name
+      email
+      address
+      city
+      zipcode
+    }
+  }
+`
+
+const useStyles = makeStyles(theme => ({
+  background: {
+    height: '100vh',
+    backgroundColor: '#266863',
+  },
+  title: {
+    margin: '20px 0',
+    color: '#fff',
+  },
+}))
 
 const UserProfile = () => {
-  return (
-    <div>
-      <div>Profile Page</div>
-    </div>
-  )
+  const { error, loading, data } = useQuery(GET_SCHOOL_INFO)
+  const classes = useStyles()
+
+  if (error) {
+    return <ErrorMessage />
+  }
+  if (loading) {
+    return <Spinner />
+  }
+  if (data) {
+    const { school } = data
+
+    return (
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        className={classes.background}
+      >
+        <Grid item>
+          <Typography component='h1' variant='h2' className={classes.title}>
+            Update Information
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Card>
+            <Grid container direction='column' spacing={5}>
+              <Grid item>
+                <EditProfile school={school} />
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    )
+  }
 }
 
 export default UserProfile
-
-//? email
-//? password
-//? school name
-//? address
-//? city
-//? zip code
